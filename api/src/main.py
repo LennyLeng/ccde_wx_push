28e951efc4e125fa7a075db5e716472898dcd4ab
LennyLeng <admin@lennyleng.com>
2016-07-21 18:54:55 +0800

    add api

---
+++ api/src/main.py
@@ -0,0 +1,53 @@
+#encoding=utf8
+#!flask/bin/python
+
+from flask import Flask, request
+import mydb
+import json
+import redis
+import pdb
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def push_task():
+
+    try:
+        post_data = json.loads(request.data)
+        db = mydb.mysql_driver()
+        app = db.read('SELECT * FROM app where app_id = %d' % (post_data['app_id']))
+        app = app[0]
+
+        if request.remote_addr != app['app_server_ip']:
+            raise Exception('ip addr not match! your ip : %s' % (request.remote_addr))
+
+        db.write(
+            "INSERT INTO log(log_push_type, log_push_target, log_push_text, app_id) VALUES ('%s' ,'%s', '%s', %d)" %
+            (post_data['push_type'], post_data['push_target'], post_data['push_content'], app['app_id'])
+        )
+
+        log_id = db.write_ret_val()
+    except Exception as e:
+        ret_data = {'db_status' : False, 'cache_status' : False, 'error_info' : e.message}
+        return json.dumps(ret_data, ensure_ascii=False)
+
+    try:
+        push_data = {}
+        push_data['log_id'] = log_id
+        push_data['app_id'] = app['app_id']
+        push_data['log_push_type'] = post_data['push_type']
+        push_data['push_target'] = post_data['push_target']
+        push_data['push_content'] = post_data['push_content']
+
+        r = redis.Redis(os.getenv('CACHE_HOST'))
+        r.publish(app['app_channel'], json.dumps(push_data, ensure_ascii=False))
+    except Exception as e:
+        ret_data = {'db_status' : True, 'cache_status' : False, 'error_info' : e.message, 'log_id' : log_id}
+        return json.dumps(ret_data, ensure_ascii=False)
+
+    ret_data = {'db_status' : True, 'cache_status' : True, 'log_id' : log_id}
+    return json.dumps(ret_data, ensure_ascii=False)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
---
+++ api/src/mydb.py
@@ -0,0 +1,46 @@
+#encoding=utf8
+import MySQLdb
+import os
+
+class MysqlDriverException(Exception):
+    def __init__(self, code, msg):
+        Exception.__init__(self)
+        self.code = code
+        self.message = msg
+
+class mysql_driver():
+    def __init__(self):
+        try:
+            #self.conn = psycopg2.connect(database=os.getenv('DB_NAME'), user=os.getenv('DB_LOGIN_NAME'), password=os.getenv('DB_LOGIN_PASS'), host=os.getenv('DB_HOST'))
+            self.conn = MySQLdb.connect(host=os.getenv('DB_HOST'), user=os.getenv('DB_USER'), passwd=os.getenv('DB_PASSWD'), db=os.getenv('DB_NAME'), charset='utf8')
+            self.cur = self.conn.cursor()
+        except Exception as e:
+            raise MysqlDriverException(500, ("数据库链接失败:%s" % e))
+
+    def __del__(self):
+        try:
+            self.conn.close()
+        except Exception as e:
+            pass
+
+    def read(self, sql):
+        try:
+            self.cur.execute(sql)
+            return [dict((self.cur.description[i][0], value) for i, value in enumerate(row)) for row in self.cur.fetchall()]
+        except Exception as e:
+            raise MysqlDriverException(500, ("数据库读取失败:%s" % e))
+
+    def write(self ,sql):
+        try:
+            self.cur.execute(sql)
+            self.conn.commit()
+        except Exception as e:
+            self.conn.rollback()
+            raise MysqlDriverException(500, ("数据库写入失败:%s" % e))
+
+    def write_ret_val(self):
+        try:
+            self.cur.execute('SELECT LAST_INSERT_ID()')
+            return self.cur.fetchone()[0]
+        except Exception as e:
+            raise MysqlDriverException(500, ("数据库写入返回值失败:%s" % e))
